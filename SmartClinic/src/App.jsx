bf02447e65f3c4881c05d5ec96ccfae45fa70178
Prudhvi-232 <prudhvitaduvai@gmail.com>
2025-08-03 20:45:51 +0530

    improve code formatting in App

--- SmartClinic/src/App.jsx
+++ SmartClinic/src/App.jsx
@@ -1,87 +1,94 @@
-import './App.css'
-import Navbar from './Components/Navbar'
-import Hero from './Pages/Hero'
-import LoginPage from './Pages/LoginPage'
-import SignUpPage from './Pages/SignUpPage'
-import ChangePassword from './Pages/ChangePassword'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Route, Routes } from 'react-router-dom'
-import { useRef } from 'react';
-import PageNotFound from './Pages/PageNotFound';
-import Doctors from './Pages/Doctors';
-import DiagnosesPage from './Pages/DiagnosesPage';
-import ForgotPassword from './Pages/ForgotPassword';
-import VoiceRecorder from './components/VoiceRecorder';
-import Admin from './pages/Admin';
-import AdminUser from './pages/AdminUser'
-import AdminAudio from './pages/AdminAudio'
-import AdminAudioFilter from './pages/AdminAudioFilter'
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Hero from "./Pages/Hero";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import ChangePassword from "./Pages/ChangePassword";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Route, Routes } from "react-router-dom";
+import { useRef } from "react";
+import PageNotFound from "./Pages/PageNotFound";
+import Doctors from "./Pages/Doctors";
+import DiagnosesPage from "./Pages/DiagnosesPage";
+import ForgotPassword from "./Pages/ForgotPassword";
+import VoiceRecorder from "./Components/VoiceRecorder";
+import Admin from "./Pages/Admin";
+import AdminUser from "./Pages/AdminUser";
+import AdminAudio from "./Pages/AdminAudio";
+import AdminAudioFilter from "./Pages/AdminAudioFilter";
 function App() {
-  const HomeSection = useRef(null);
-  const AboutSection = useRef(null);
-  const ServicesPageSection = useRef(null);
-  const WhyChooseUsSection = useRef(null);
-  const CarousalPageSection = useRef(null);
-  const ConnectWithUsSection = useRef(null);
-  const FooterSection = useRef(null);
-  const jwt = sessionStorage.getItem('jwt');
-  const encryptedData=sessionStorage.getItem('encryptedData');
-  return (
-    <div >
-    
-      <Navbar message={"login successful"}
-        AboutSection={AboutSection}
-        HomeSection={HomeSection}
-        ServicesPageSection={ServicesPageSection}
-        WhyChooseUsSection={WhyChooseUsSection}
-        CarousalPageSection={CarousalPageSection}
-        ConnectWithUsSection={ConnectWithUsSection}
-        FooterSection={FooterSection}
-      />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
+	const HomeSection = useRef(null);
+	const AboutSection = useRef(null);
+	const ServicesPageSection = useRef(null);
+	const WhyChooseUsSection = useRef(null);
+	const CarousalPageSection = useRef(null);
+	const ConnectWithUsSection = useRef(null);
+	const FooterSection = useRef(null);
+	const jwt = sessionStorage.getItem("jwt");
+	const encryptedData = sessionStorage.getItem("encryptedData");
+	return (
+		<div>
+			<Navbar
+				message={"login successful"}
+				AboutSection={AboutSection}
+				HomeSection={HomeSection}
+				ServicesPageSection={ServicesPageSection}
+				WhyChooseUsSection={WhyChooseUsSection}
+				CarousalPageSection={CarousalPageSection}
+				ConnectWithUsSection={ConnectWithUsSection}
+				FooterSection={FooterSection}
+			/>
+			<ToastContainer
+				position="top-right"
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="dark"
+			/>
 
-      <Routes>
-        <Route path='/'
-          element={
-            <Hero
-              AboutSection={AboutSection}
-              HomeSection={HomeSection}
-              ServicesPageSection={ServicesPageSection}
-              WhyChooseUsSection={WhyChooseUsSection}
-              CarousalPageSection={CarousalPageSection}
-              ConnectWithUsSection={ConnectWithUsSection}
-              FooterSection={FooterSection}
-            />} />
-        <Route path='/hospitals' element={<Doctors />} />
-        <Route path='/diagnoses' element={<DiagnosesPage />} />
-        <Route path='/forgetPassword' element={<ForgotPassword/>} />
-        {!jwt && <Route path='/login' element={<LoginPage />} />}
-        {!jwt && <Route path='/signup' element={<SignUpPage />} />}
-        {encryptedData && <Route path='/changePass' element={<ChangePassword />} />}
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Hero
+							AboutSection={AboutSection}
+							HomeSection={HomeSection}
+							ServicesPageSection={ServicesPageSection}
+							WhyChooseUsSection={WhyChooseUsSection}
+							CarousalPageSection={CarousalPageSection}
+							ConnectWithUsSection={ConnectWithUsSection}
+							FooterSection={FooterSection}
+						/>
+					}
+				/>
+				<Route path="/hospitals" element={<Doctors />} />
+				<Route path="/diagnoses" element={<DiagnosesPage />} />
+				<Route path="/forgetPassword" element={<ForgotPassword />} />
+				{!jwt && <Route path="/login" element={<LoginPage />} />}
+				{!jwt && <Route path="/signup" element={<SignUpPage />} />}
+				{encryptedData && (
+					<Route path="/changePass" element={<ChangePassword />} />
+				)}
 
-        <Route path='*' element={<PageNotFound />} />
-        <Route path="/admin-dashboard" element= {<Admin/>}/>
-        <Route path="/admin-users"  element={<AdminUser/>} />
-        <Route path="/admin-audio" element={<AdminAudio/>} />
-        <Route path="/admin-audio-filter" element={<AdminAudioFilter/>}/>
-      </Routes>
+				<Route path="*" element={<PageNotFound />} />
+				<Route path="/admin-dashboard" element={<Admin />} />
+				<Route path="/admin-users" element={<AdminUser />} />
+				<Route path="/admin-audio" element={<AdminAudio />} />
+				<Route
+					path="/admin-audio-filter"
+					element={<AdminAudioFilter />}
+				/>
+			</Routes>
 
-      <VoiceRecorder />
-
-    </div>
-  )
+			<VoiceRecorder />
+		</div>
+	);
 }
 
 export default App;
